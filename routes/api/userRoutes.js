027af4af32ea08257a203bb4bf2007d692e61ca0
Julio Bermudez <julioph0n3@gmail.com>
2023-10-23 01:06:49 -0400

    added all the folders and renamed the api routes, controller and model

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+  } = require('../../controllers/userController');
+
+//http://localhost:3001/api/users/
+router.route('/').get(getUsers).post(createUser);
+
+//http://localhost:3001/api/users/:userId
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser)
+;
+
+//http://localhost:3001/api/users/:userId/friends/:friendId
+router.route('/:userId/')
+.post(addFriend)
+.delete(removeFriend)
+module.exports = router;
